42b939c85c1ed3dc06def33500a5a63a7f9c5489
Eduardo Oliveira <edo@axogroup.com>
2025-04-02 09:47:59 +0100

    added open api lint

--- init.lua
+++ init.lua
@@ -608,7 +608,12 @@ require('lazy').setup({
       --  So, we create new capabilities with nvim cmp, and then broadcast that to the servers.
       local capabilities = vim.lsp.protocol.make_client_capabilities()
       capabilities = vim.tbl_deep_extend('force', capabilities, require('cmp_nvim_lsp').default_capabilities())
-
+      vim.filetype.add {
+        pattern = {
+          ['openapi.*%.ya?ml'] = 'yaml.openapi',
+          ['openapi.*%.json'] = 'json.openapi',
+        },
+      }
       -- Enable the following language servers
       --  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
       --
@@ -632,7 +637,7 @@ require('lazy').setup({
         ts_ls = {},
         --
         jqls = {},
-
+        vacuum = {},
         lua_ls = {
           -- cmd = { ... },
           -- filetypes = { ... },
